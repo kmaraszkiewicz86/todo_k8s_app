3601bb899fa861f2cb5fbb4d84a00fa89c963034
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2025-02-21 09:38:27 +0100

    Added sample blazor project

---
+++ src/UI/Blazor/ToDo/ToDo/Program.cs
@@ -0,0 +1,30 @@
+using ToDo.Client.Pages;
+using ToDo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(ToDo.Client._Imports).Assembly);
+
+app.Run();
