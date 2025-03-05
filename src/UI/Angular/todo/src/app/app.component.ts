e53ae5f989ec38bf4bfd771b646e6a802583e07f
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2025-03-05 09:07:38 +0100

    Added basic structure of angular app

---
+++ src/UI/Angular/todo/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component  } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { BrowserModule } from '@angular/platform-browser';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    BrowserModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'todo';
+}
